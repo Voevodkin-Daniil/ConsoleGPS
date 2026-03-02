05363f079784bf8f7949080aa991cd858f316470
Voevodkin Daniil <voevoda.200@yandex.ru>
2026-03-02 16:29:04 +0400

    Добавил плоскую проекцию и тест в консоли на определение значений

--- ConsoleGPS/ParserGMS.cs
+++ ConsoleGPS/ParserGMS.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
+
 
 namespace ConsoleGPS
 {
@@ -56,4 +52,223 @@ namespace ConsoleGPS
             return $"{sign}{degrees}°{minutes:00}'{seconds:00.00}\"";
         }
     }
+
+    #region УДАЛИТЬ ПОТОМ
+    /// <summary>
+    /// Предоставляет методы для триангуляции координат Наблюдателя и цели на основе данных с двух наблюдательных пунктов.
+    /// </summary>
+    public static class TargetLocator
+    {
+        /// <summary>
+        /// Вычисляет положительный остаток от деления (математический модуль).
+        /// </summary>
+        /// <param name="x">Делимое</param>
+        /// <param name="m">Модуль (делитель)</param>
+        /// <returns>Положительное значение остатка от деления</returns>
+        private static double Mod(double x, double m)
+        {
+            double result = x % m;
+            return result < 0 ? result + m : result;
+        }
+
+        /// <summary>
+        /// Вычисляет среднюю точку и расстояние между двумя точками на плоскости.
+        /// </summary>
+        /// <param name="point1">Координаты первой точки (X, Y, Z)</param>
+        /// <param name="point2">Координаты второй точки (X, Y, Z)</param>
+        /// <returns>Кортеж, содержащий координаты средней точки и расстояние между исходными точками</returns>
+        public static (double X, double Y, double L) AveragePoints((double X, double Y, double Z) point1, (double X, double Y, double Z) point2)
+        {
+            return
+            (
+                (point1.X + point2.X) / 2,
+                (point1.Y + point2.Y) / 2,
+                Math.Sqrt(Math.Pow(point1.X - point2.X, 2) + Math.Pow(point1.Y - point2.Y, 2))
+            );
+        }
+
+        /// <summary>
+        /// Вычисляет среднюю точку и расстояние между двумя точками на плоскости.
+        /// </summary>
+        /// <param name="point1">Координаты первой точки (X, Y)</param>
+        /// <param name="point2">Координаты второй точки (X, Y)</param>
+        /// <returns>Кортеж, содержащий координаты средней точки и расстояние между исходными точками</returns>
+        public static (double X, double Y, double L) AveragePoints((double X, double Y) point1, (double X, double Y) point2)
+            => AveragePoints((point1.X, point1.Y, 0), (point2.X, point2.Y, 0));
+
+        /// <summary>
+        /// Находит точки пересечения двух окружностей на плоскости.
+        /// </summary>
+        /// <param name="x1">X-координата центра первой окружности</param>
+        /// <param name="y1">Y-координата центра первой окружности</param>
+        /// <param name="x2">X-координата центра второй окружности</param>
+        /// <param name="y2">Y-координата центра второй окружности</param>
+        /// <param name="radius1">Радиус первой окружности</param>
+        /// <param name="radius2">Радиус второй окружности</param>
+        /// <param name="height">Z-координата точек пересечения (по умолчанию 0)</param>
+        /// <returns>Две точки пересечения окружностей с заданной высотой</returns>
+        /// <exception cref="Exception">Возникает, если окружности не пересекаются или совпадают</exception>
+        public static ((double X, double Y, double Z) A, (double X, double Y, double Z) B) FindCircleIntersection(
+            double x1, double y1, double x2, double y2,
+            double radius1, double radius2, double height = 0)
+        {
+            double dx = x2 - x1;
+            double dy = y2 - y1;
+            double distance = Math.Sqrt(dx * dx + dy * dy);
+
+            // Проверка геометрической возможности пересечения
+            if (distance > radius1 + radius2)
+                throw new Exception("Окружности не пересекаются: расстояние между центрами превышает сумму радиусов");
+
+            if (distance < Math.Abs(radius1 - radius2))
+                throw new Exception("Окружности не пересекаются: одна окружность находится полностью внутри другой");
+
+            if (distance == 0 && radius1 == radius2)
+                throw new Exception("Окружности совпадают: бесконечное множество точек пересечения");
+
+            // Вычисление параметров точек пересечения
+            double a = (Math.Pow(radius1, 2) - Math.Pow(radius2, 2) + Math.Pow(distance, 2)) / (2 * distance);
+            double h = Math.Sqrt(Math.Pow(radius1, 2) - Math.Pow(a, 2));
+
+            double x_mid = x1 + a * dx / distance;
+            double y_mid = y1 + a * dy / distance;
+
+            // Формирование двух точек пересечения
+            return (
+                (x_mid - h * dy / distance, y_mid + h * dx / distance, height),
+                (x_mid + h * dy / distance, y_mid - h * dx / distance, height)
+                );
+        }
+
+        /// <summary>
+        /// Вычисляет координаты Наблюдателя и наземной цели методом триангуляции по данным с двух наблюдателей.
+        /// </summary>
+        /// <param name="O1_X">Координата X первого опорного оъекта</param>
+        /// <param name="O1_Y">Координата Y первого опорного оъекта</param>
+        /// <param name="O1_Z">Высота первого опорного оъекта</param>
+        /// <param name="O2_X">Координата X второго опорного оъекта</param>
+        /// <param name="O2_Y">Координата Y второго опорного оъекта</param>
+        /// <param name="O2_Z">Высота второго опорного оъекта</param>
+        /// <param name="L1">Наклонная дальность от первого опорного оъекта до Наблюдателя</param>
+        /// <param name="L2">Наклонная дальность от второго опорного оъекта до Наблюдателя</param>
+        /// <param name="L3">Наклонная дальность от Наблюдателя до цели</param>
+        /// <param name="a">Горизонтальный угол между направлением на цель и линией О1-Наблюдатель (в градусах)</param>
+        /// <param name="b">Горизонтальный угол между направлением на цель и линией О2-Наблюдатель (в градусах)</param>
+        /// <param name="aa">Угол места от первого опорного объекта на Наблюдатель (в градусах)</param>
+        /// <param name="bb">Угол места от второго опорного объекта на Наблюдатель (в градусах)</param>
+        /// <param name="c">Угол места от Наблюдателя на цель (в градусах)</param>
+        /// <returns>
+        /// Кортеж из двух точек:
+        /// - A: координаты Наблюдателя (X, Y, Z)
+        /// - B: координаты цели (X, Y, Z)
+        /// </returns>
+        public static ((double X, double Y, double Z) A, (double X, double Y, double Z) B) GetTargetCoordinates(
+            double O1_X, double O1_Y, double O1_Z, double O2_X, double O2_Y, double O2_Z,
+            double L1, double L2, double L3, double a, double b, double aa, double bb, double c)
+        {
+            // Расчёт высоты Наблюдателя как среднего значения от двух наблюдателей
+            double H = ((O1_Z - Math.Sin(aa * Math.PI / 180) * L1) +
+                        (O2_Z - Math.Sin(bb * Math.PI / 180) * L2)) / 2;
+
+            // Горизонтальные проекции дальностей
+            double l1 = L1 * Math.Cos(aa * Math.PI / 180);
+            double l2 = L2 * Math.Cos(bb * Math.PI / 180);
+
+            // Определение местоположения Наблюдателя (пересечение двух окружностей)
+            var BPLA2 = FindCircleIntersection(O1_X, O1_Y, O2_X, O2_Y, l1, l2, 0);
+            // Выбор правильного решения на основе разности углов
+            var BPLA = (Mod((Math.PI + Math.PI * (a - b) / 180), (2 * Math.PI)) - Math.PI) > 0 ? BPLA2.B : BPLA2.A;
+
+            // Горизонтальная проекция дальности от Наблюдателя до цели
+            double l3 = L3 * Math.Cos(c * Math.PI / 180);
+
+            // Расчёт горизонтальных дальностей от наблюдателей до цели
+            double r1 = Math.Sqrt(l1 * l1 + l3 * l3 - 2 * l1 * l3 * Math.Cos(Math.PI * Math.Abs(a) / 180));
+            double r2 = Math.Sqrt(l2 * l2 + l3 * l3 - 2 * l2 * l3 * Math.Cos(Math.PI * Math.Abs(b) / 180));
+
+            // Нахождение возможных позиций цели относительно Наблюдателя и опорных объектов
+            var points1 = FindCircleIntersection(BPLA.X, BPLA.Y, O1_X, O1_Y, l3, r1);
+            var points2 = FindCircleIntersection(BPLA.X, BPLA.Y, O2_X, O2_Y, l3, r2);
+
+            // Поиск наилучшего решения методом усреднения всех комбинаций
+            var avgPoints = new List<(double X, double Y, double L)>
+    {
+        AveragePoints(points1.A, points2.A),
+        AveragePoints(points1.A, points2.B),
+        AveragePoints(points1.B, points2.A),
+        AveragePoints(points1.B, points2.B)
+    };
+
+            // Выбор комбинации с минимальным расстоянием между точками (наиболее согласованное решение)
+            avgPoints.Sort((p1, p2) => p1.L.CompareTo(p2.L));
+            var bestMatch = avgPoints[0];
+
+            return
+            (
+                (BPLA.X, BPLA.Y, H), // Координаты Наблюдателя
+                (bestMatch.X, bestMatch.Y, H + L3 * Math.Sin(c * Math.PI / 180)) // Координаты цели
+            );
+        }
+
+        /// <summary>
+        /// Альтернативный метод вычисления координат цели с использованием векторного вращения.
+        /// </summary>
+        /// <param name="O1_X">Координата X первого опорного оъекта</param>
+        /// <param name="O1_Y">Координата Y первого опорного оъекта</param>
+        /// <param name="O1_Z">Высота первого опорного оъекта</param>
+        /// <param name="O2_X">Координата X второго опорного оъекта</param>
+        /// <param name="O2_Y">Координата Y второго опорного оъекта</param>
+        /// <param name="O2_Z">Высота второго опорного оъекта</param>
+        /// <param name="L1">Наклонная дальность от первого опорного оъекта до Наблюдателя</param>
+        /// <param name="L2">Наклонная дальность от второго опорного оъекта до Наблюдателя</param>
+        /// <param name="L3">Наклонная дальность от Наблюдателя до цели</param>
+        /// <param name="a">Горизонтальный угол между направлением на цель и линией О1-Наблюдатель (в градусах)</param>
+        /// <param name="b">Горизонтальный угол между направлением на цель и линией О2-Наблюдатель (в градусах)</param>
+        /// <param name="aa">Угол места от первого опорного оъекта на Наблюдатель (в градусах)</param>
+        /// <param name="bb">Угол места от второго опорного оъекта на Наблюдатель (в градусах)</param>
+        /// <param name="c">Угол места от Наблюдателя на цель (в градусах)</param>
+        /// <returns>
+        /// Кортеж из двух точек:
+        /// - A: координаты Наблюдатель (X, Y, Z)
+        /// - B: координаты цели (X, Y, Z)
+        /// </returns>
+        public static ((double X, double Y, double Z) A, (double X, double Y, double Z) B) GetTargetCoordinatesAlternative(
+            double O1_X, double O1_Y, double O1_Z, double O2_X, double O2_Y, double O2_Z,
+            double L1, double L2, double L3, double a, double b, double aa, double bb, double c)
+        {
+            // Расчёт высоты Наблюдателя как среднего значения от двух наблюдателей
+            double H = ((O1_Z - Math.Sin(aa * Math.PI / 180) * L1) +
+                        (O2_Z - Math.Sin(bb * Math.PI / 180) * L2)) / 2;
+
+            // Горизонтальные проекции дальностей от опорных объектов до Наблюдателя
+            double l1 = L1 * Math.Cos(aa * Math.PI / 180);
+            double l2 = L2 * Math.Cos(bb * Math.PI / 180);
+
+            // Определение местоположения Наблюдателя (пересечение двух окружностей)
+            var BPLA2 = FindCircleIntersection(O1_X, O1_Y, O2_X, O2_Y, l1, l2, 0);
+            var BPLA = (Mod((Math.PI + Math.PI * (a - b) / 180), (2 * Math.PI)) - Math.PI) > 0 ? BPLA2.B : BPLA2.A;
+
+            // Горизонтальная проекция дальности от Наблюдателя до цели
+            double l3 = L3 * Math.Cos(c * Math.PI / 180);
+
+            // Векторный метод: вращение векторов от Наблюдателя к наблюдателям на заданные углы
+            double x1 = O1_X - BPLA.X;
+            double y1 = O1_Y - BPLA.Y;
+            double x2 = O2_X - BPLA.X;
+            double y2 = O2_Y - BPLA.Y;
+
+            // Поворот векторов и масштабирование для получения координат цели
+            double rez_X1 = BPLA.X + l3 * (x1 * Math.Cos(Math.PI * -a / 180) - y1 * Math.Sin(Math.PI * -a / 180)) / l1;
+            double rez_X2 = BPLA.X + l3 * (x2 * Math.Cos(Math.PI * -b / 180) - y2 * Math.Sin(Math.PI * -b / 180)) / l2;
+            double rez_Y1 = BPLA.Y + l3 * (x1 * Math.Sin(Math.PI * -a / 180) + y1 * Math.Cos(Math.PI * -a / 180)) / l1;
+            double rez_Y2 = BPLA.Y + l3 * (x2 * Math.Sin(Math.PI * -b / 180) + y2 * Math.Cos(Math.PI * -b / 180)) / l2;
+
+            return
+            (
+                (BPLA.X, BPLA.Y, H), // Координаты Наблюдатель
+                ((rez_X1 + rez_X2) / 2, (rez_Y1 + rez_Y2) / 2, H + L3 * Math.Sin(c * Math.PI / 180)) // Координаты цели (усреднение двух решений)
+            );
+        }
+    }
+    #endregion
 }
--- ConsoleGPS/Program.cs
+++ ConsoleGPS/Program.cs
@@ -17,7 +17,10 @@ class Program
 
         // Проерка что выдаст растояние
         test_2(OO1, OO2);
+        test_3(OO2, OO1);
 
+        // Сравнение ответов при преобразовании
+        test_4();
 
     }
 
@@ -29,6 +32,7 @@ class Program
         var temp2 = TranslationWGS84.ConvertToGaussKrueger(point_1.B, point_1.L);
 
         Console.WriteLine(Math.Sqrt(Math.Pow(temp.x - temp2.x,2) + Math.Pow(temp.y - temp2.y, 2)));
+        Console.WriteLine();
 
         return true;
     }
@@ -39,6 +43,94 @@ class Program
         var temp_B = TranslationWGS84.ConvertToGaussKrueger(B.B, B.L);
 
         Console.WriteLine(Math.Sqrt(Math.Pow(temp_A.x - temp_B.x, 2) + Math.Pow(temp_A.y - temp_B.y, 2)));
+        Console.WriteLine();
+
+        return true;
+    }
+
+    private static bool test_3((double B, double L, double H) T, (double B, double L, double H) OO)
+    {
+        var temp_T = TranslationWGS84.ConvertGeodeticToLocalTangent(T, OO);
+
+        
+        Console.WriteLine(Math.Sqrt(Math.Pow(temp_T.east, 2) + Math.Pow(temp_T.north, 2)));
+
+        var T_new = TranslationWGS84.ConvertLocalTangentToGeodetic(temp_T, OO);
+        Console.WriteLine($"{(T.B / Math.PI * 180):f7} \t {(T.L / Math.PI * 180):f7}");
+        Console.WriteLine($"{(T_new.B / Math.PI * 180):f7} \t {(T_new.L / Math.PI * 180):f7}");
+        Console.WriteLine();
+
+        return true;
+    }
+
+    private static bool test_4()
+    {
+        Console.WriteLine("Тест 4");
+        // Начальные значения
+        double OO1_B = 53.2015043 / 180 * Math.PI;
+        double OO1_L = 50.1133423 / 180 * Math.PI;
+        double OO1_H = -10;
+        double OO2_B = 53.2000976 / 180 * Math.PI;
+        double OO2_L = 50.1125830 / 180 * Math.PI;
+        double OO2_H = 13;
+        double L1 = 210.01;
+        double L2 = 247.08;
+        double L3 = 246.46;
+
+        double a = -18.32;
+        double b = 41.07;
+        double aa = -58.99;
+        double bb = -39.45;
+        double cc = -37.48;
+
+
+        // Проверка на проекции Гаусса-Крюгера
+        var temp_OO1_1 = TranslationWGS84.ConvertToGaussKrueger(OO1_B, OO1_L);
+        var temp_OO2_1 = TranslationWGS84.ConvertToGaussKrueger(OO2_B, OO2_L);
+
+        (var B1, var T1) = TargetLocator.GetTargetCoordinates(
+                            temp_OO1_1.x, temp_OO1_1.y, OO1_H,
+                            temp_OO2_1.x, temp_OO2_1.y, OO2_H,
+                            L1, L2, L3, a, b, aa, bb, cc);
+
+        var gB1 = TranslationWGS84.ConvertFromGaussKrueger(B1.X, B1.Y);
+        var gT1 = TranslationWGS84.ConvertFromGaussKrueger(T1.X, T1.Y);
+
+        Console.WriteLine($"B = {(gB1.B / Math.PI * 180):f7}, {(gB1.L / Math.PI * 180):f7}, {B1.Z:f2}");
+        Console.WriteLine($"T = {(gT1.B / Math.PI * 180):f7}, {(gT1.L / Math.PI * 180):f7}, {T1.Z:f2}");
+        Console.WriteLine();
+
+        // Проверка на Координатах локальной косательной плоскости
+
+        var temp_OO1_2 = TranslationWGS84.ConvertGeodeticToLocalTangent(
+            (OO1_B, OO1_L, OO1_H),
+            (OO1_B, OO1_L, OO1_H));
+        var temp_OO2_2 = TranslationWGS84.ConvertGeodeticToLocalTangent(
+            (OO2_B, OO2_L, OO2_H),
+            (OO1_B, OO1_L, OO1_H));
+
+        /*
+         * 
+         * ЗАМЕЧАНИЕ
+         * Метод GetTargetCoordinates работает при условии что x - направление на Север, y - Направление на Восток
+         * 
+         */
+
+        (var B2, var T2) = TargetLocator.GetTargetCoordinates(
+                            temp_OO1_2.north, temp_OO1_2.east, temp_OO1_2.up,
+                            temp_OO2_2.north, temp_OO2_2.east, temp_OO2_2.up,
+                            L1, L2, L3, a, b, aa, bb, cc);
+
+        var gB2 = TranslationWGS84.ConvertLocalTangentToGeodetic((B2.Y, B2.X, B2.Z), (OO1_B, OO1_L, OO1_H));
+        var gT2 = TranslationWGS84.ConvertLocalTangentToGeodetic((T2.Y, T2.X, T2.Z), (OO1_B, OO1_L, OO1_H));
+
+        Console.WriteLine($"B = {(gB2.B / Math.PI * 180):f7}, {(gB2.L / Math.PI * 180):f7}, {gB2.H:f2}");
+        Console.WriteLine($"T = {(gT2.B / Math.PI * 180):f7}, {(gT2.L / Math.PI * 180):f7}, {gT2.H:f2}");
+        Console.WriteLine();
+
+
+
+        Console.WriteLine();
 
         return true;
     }
